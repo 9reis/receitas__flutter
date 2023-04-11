936db96305225f94e139b259c7b9ccf2353f4b1a
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-11 17:50:28 -0300

    Selecionando Refeicao por Categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:receitas__flutter/data/dummy_data.dart';
 import 'package:receitas__flutter/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -9,15 +10,24 @@ class CategoriesMealsScreen extends StatelessWidget {
     // Pega os dados das categorias através dos argumentos passados na rota
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
+    // Percorre a lista de refeicoes e pega cada item
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      // Pega cada refeição de acordo com a categoria
+      // clicada na lista de categorias
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        // ONDE IRÁ COLOCAR OS TRECHOS DAS HISTORIAS
-        child: Text('Receitas por Categoria - ${category.id}'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          // Total de itens na lista de refeições
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+        ));
   }
 }
