c57d6650b4ba2adfdcd51e97925774f01a2900e8
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-12 12:45:00 -0300

    Adicionando TabBar na AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:receitas__flutter/screens/categories_meals_screen.dart';
 import 'package:receitas__flutter/screens/categories_screen.dart';
 import 'package:receitas__flutter/screens/meal_detail_screen.dart';
+import 'package:receitas__flutter/screens/tabs_screen.dart';
 import 'package:receitas__flutter/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -27,31 +28,10 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         // Define o CategoriesScreen como rota inicial
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
-      // Só entra nessa metodo caso a rota que tentou acessa não exista
-      // Recebe a rota que tentou acessar
-      // onGenerateRoute: ((settings) {
-      //   // Func utilizada para definir rotas dinamicamente
-      //   if (settings.name == '/alguma-coisa') {
-      //     return null;
-      //   } else if (settings.name == '/outra-coisa') {
-      //     return null;
-      //   } else {
-      //     return MaterialPageRoute(builder: (_) {
-      //       return CategoriesScreen();
-      //     });
-      //   }
-      // }),
-
-      //Metodo parecido com o 404
-      onUnknownRoute: ((settings) {
-        return MaterialPageRoute(builder: (_) {
-          return CategoriesScreen();
-        });
-      }),
     );
   }
 }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:receitas__flutter/screens/categories_screen.dart';
+import 'package:receitas__flutter/screens/favorite_screen.dart';
+
+class TabsScreen extends StatelessWidget {
+  const TabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      // Quantidade de abas
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Vamos Cozinhar ?'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categorias',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favoritos',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
